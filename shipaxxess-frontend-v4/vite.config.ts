3e87cb3725664532ca14659a23d1c9f1555c17ec
Haseeb Mustafa <haseb842@gmail.com>
2024-09-23 11:05:12 +0500

    updated

--- shipaxxess-frontend-v4/vite.config.ts
+++ shipaxxess-frontend-v4/vite.config.ts
@@ -5,4 +5,20 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react(), tsconfigPaths()],
-});
+	build: {
+		chunkSizeWarningLimit: 1000,
+		rollupOptions: {
+		  output: {
+			manualChunks(id) {
+			  if (id.includes('node_modules')) {
+				return id
+				  .toString()
+				  .split('node_modules/')[1]
+				  .split('/')[0]
+				  .toString();
+			  }
+			},
+		  },
+		},
+	  },
+	});
